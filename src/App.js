13e33afa60866d79cd52b19c7744d71f9aea3559
pucho <fran.mazzoni@gmail.com>
2018-04-01 14:29:59 -0300

    4square places

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { geolocated } from 'react-geolocated';
+import axios from 'axios';
+import qs from 'qs';
 
 import logo from './logo.svg';
 
@@ -12,17 +15,51 @@ class App extends Component {
     className: PropTypes.string
   };
 
+  state = {
+    restaurants: []
+  };
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps.coords);
+    if (nextProps.coords) {
+      this.getRestaurantsByLoc(nextProps.coords);
+    }
+  }
+
+  getRestaurantsByLoc = coords => {
+    const queryString = qs.stringify({
+      client_id: process.env.REACT_APP_FS_CLIENTID,
+      client_secret: process.env.REACT_APP_FS_CLIENTSECRET,
+      ll: `${coords.latitude}, ${coords.longitude}`,
+      v: '20180323'
+    });
+    axios
+      .get(`https://api.foursquare.com/v2/venues/search?${queryString}`)
+      .then(data => {
+        this.setState({ restaurants: data.data.response.venues });
+      });
+  };
   render() {
+    console.log(this.props);
     return (
       <div className={this.props.className}>
         <TopBar />
+        {this.state.restaurants.length > 0 &&
+          this.state.restaurants.map(restaurant => {
+            return <Places key={restaurant.id} name={restaurant.name} />;
+          })}
       </div>
     );
   }
 }
 
-export default styled(App)`
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false
+  },
+  userDecisionTimeout: 300
+})(styled(App)`
   background-color: #3d3dd8;
   color: white;
   height: 100vh;
-`;
+`);
